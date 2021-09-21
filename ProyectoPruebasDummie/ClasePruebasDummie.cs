b4d639a1ad63367646ee59cf82e3feacc2a83e5c
Carlos E Benitez A <Carlos E Benitez A@DESKTOP-4PBAGG4>
2021-09-21 17:55:35 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoPruebasDummie/ClasePruebasDummie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProyectoPruebasDummie
+{
+    [TestClass]
+    public class ClasePruebasDummie
+    {
+        [TestMethod]
+        public void Metodo1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Metodo2()
+        {
+            Assert.IsFalse(false);
+        }
+
+    }
+}
---
+++ ProyectoPruebasDummie/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProyectoPruebasDummie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProyectoPruebasDummie")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0db18a7-7cab-4cd5-948a-789ddf8ce340")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
